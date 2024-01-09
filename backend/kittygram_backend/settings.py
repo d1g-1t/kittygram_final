00e36629ff9edac8e2bc76cc0fd52ecb657cab95
Pavel Okhrim <133741770+d1g-1t@users.noreply.github.com>
2024-01-09 13:08:34 +0300

    Изменения: 1. в settings.py изменил права доступа; 2. из .env убрал DB_HOST и DB_PORT.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -103,16 +103,3 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
-    ],
-
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
-
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-
-}
