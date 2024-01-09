5db8f86015ff3c79800576c046a86e6edf9649cc
Pavel Okhrim <133741770+d1g-1t@users.noreply.github.com>
2024-01-09 13:26:48 +0300

    1. Вернул в settings стары права доступа; 2. Изменил в .env значения переменных. Выяснилось, что не запускается контейнер posgtres.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -103,3 +103,16 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated', 
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+
+}
